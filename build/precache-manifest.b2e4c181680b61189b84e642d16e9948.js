a70edbc7b1d745c3ed0146513d1a9b5427ae09b3
Harish KS <ksharish72@gmail.com>
2020-01-28 02:54:01 -0500

    Build folder

---
+++ build/precache-manifest.b2e4c181680b61189b84e642d16e9948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186283a20546ff8a290ee28b10c3dcac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe72bf2e67b95caf2f0",
+    "url": "/static/css/2.0862dc20.chunk.css"
+  },
+  {
+    "revision": "7276c72faa5ce7a3bd7d",
+    "url": "/static/css/main.ec3ba1b7.chunk.css"
+  },
+  {
+    "revision": "fbe72bf2e67b95caf2f0",
+    "url": "/static/js/2.be8ce180.chunk.js"
+  },
+  {
+    "revision": "bdc88b4b3387902d8682d5aa8e2bec18",
+    "url": "/static/js/2.be8ce180.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7276c72faa5ce7a3bd7d",
+    "url": "/static/js/main.9e01f3fb.chunk.js"
+  },
+  {
+    "revision": "3d1ff2a349e8e94c69a0",
+    "url": "/static/js/runtime-main.723f08ee.js"
+  }
+]);
